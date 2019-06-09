b57895f8bf0f17d1104a70c7260f328fb8178057
albrecht-net <jonasalbrecht9@gmail.com>
2019-06-09 13:20:30 +0200

    Datei für Updateanweisungen für ViewJournal erstellt

---
+++ update/dbViewJournal.update.sql
@@ -0,0 +1,5 @@
+-- Datenbank View Updates für die Journal Datenbank
+-- Achtung: Die Updates sind nur für die angegebene Versionserhöhung gültig und sollten nach aufsteigener Versionsnummer ausgeführt werden!
+--
+
+-- --------------------------------------------------------

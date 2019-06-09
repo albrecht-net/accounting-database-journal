0c1f2d50bb24fe6e7ed4330857ed479185210531
albrecht-net <jonasalbrecht9@gmail.com>
2019-06-09 13:58:11 +0200

    Update Hinweis

--- update/dbTablesJournal.update.sql
+++ update/dbTablesJournal.update.sql
@@ -1,5 +1,6 @@
 -- Datenbank Tabellen Updates für die Journal Datenbank
 -- Achtung: Die Updates sind nur für die angegebene Versionserhöhung gültig und sollten nach aufsteigener Versionsnummer ausgeführt werden!
+-- Die hier verwendeten Versionsnummern beziehen sich auf die Journal Datenbank. Die Kompabilität zu Accounting muss mithilfe der Versionsübersicht sichergestellt werden.
 --
 
 -- --------------------------------------------------------
